e9250eea33543332c3e7f66c33ade951f241b595
rememberlenny <lkbgift@gmail.com>
2013-10-13 14:17:18 -0400

    Saved the working index page

--- app/controllers/textout_controller.rb
+++ app/controllers/textout_controller.rb
@@ -3,10 +3,37 @@ require "twilio-ruby"
 class TextoutController < ApplicationController
  
   client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
- 
+  
+
+
   # Outgoing Caller ID you have previously validated with Twilio
  
   def index
+    client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+    @users       = User.all
+    @recreations = Recreation.all
+    @recipes     = Recipe.all
+    @coupons     = Coupon.all
+    @users.each do |n|
+      puts n.firstname
+      @signedUp = ""
+      if(n.groupCoupon === "1")
+        @signedUp += " #COUPON"
+      end
+      if(n.groupRecreation === "1")
+        @signedUp += " #FITNESS"
+      end
+      if(n.groupRecipe == "1")
+        @signedUp += " #RECIPE"
+      end
+      @textMessage = n.firstname + "! Welcome to Snipit. Your coach signed you up for daily messages for" + @signedUp +  ". Reply STOP to halt messages."
+      client.account.sms.messages.create(
+        :from => TWILIO_CONFIG['from'],
+        :to => n.phoneNumber,
+        :body => @textMessage
+      ) 
+      puts "Sent message to " + n.firstname
+    end
   end
 
   def sendtext
@@ -15,14 +42,13 @@ class TextoutController < ApplicationController
       
       # Create and send an SMS message
       friends = {
-        "+19493228496" => [
-          'name'=> 'Lenny'
-          ]
-        # ,"+15628823520" => "Matt",
-        # "+19185763165" => "Jennings",
-        # "+18482195617" => "Melody",
-        # "+16465484196" => "Mik",
-        # "+19174007014" => "Yvonne",
+        "+19493228496" => 'Lenny',
+        "+15628823520" => "Matt",
+        "+19185763165" => "Jennings",
+        "+18482195617" => "Melody",
+        "+16465484196" => "Mik"
+
+        # ,"+19174007014" => "Yvonne",
         # "+19175860229" => "Donya",
         # "+19175498582" => "Matylda",
         # "+12017075074" => "Juan",
@@ -34,7 +60,7 @@ class TextoutController < ApplicationController
         client.account.sms.messages.create(
           :from => TWILIO_CONFIG['from'],
           :to => key,
-          :body => "#{value}!, Welcome to SNIPIT. Bring Bananas!"
+          :body => "#{value}! Welcome to Snipit. Your coach signed you up for daily messages for new #RECIPES! Reply STOP to halt messages."
         ) 
         puts "Sent message to #{value}"
       end
