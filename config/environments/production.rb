c623eb66732a0e51b5b76819523cce94b70bc576
rememberlenny <lkbgift@gmail.com>
2013-10-12 22:26:23 -0400

    Fixing top nav

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ FeastHealth::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'http://snippetny.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://snipit.herokuapp.com/' }
 end
